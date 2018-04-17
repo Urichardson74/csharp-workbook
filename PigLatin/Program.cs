c984b8f369d60bd41a43042cc7b92464d83f870f
Ukiah Richardson <urichardson74@gmail.com>
2018-04-16 21:35:08 -0500

    Piglatin fixes

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -1,52 +1,48 @@
-﻿using System;
+
+using System;
 
 namespace PigLatin
 {
     class Program
     {
         public static void Main()
-        {
-            // your code goes here
-            Console.WriteLine("Please enter a word");
-            string word =Console.ReadLine();
-            int = vowelIndex = -1;
-
-            if ( ( word.IndexOf('a') > -1 && word.IndexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
-            vowelIndex = word.indexOf('a');
+        {   
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            Console.WriteLine("Please enter a word: ");
+            string word = Console.ReadLine();
+            int vowelIndex = -1;
+            if ((word.IndexOf('a') > -1 && word.IndexOf('a') < vowelIndex) || vowelIndex == -1) {
+                vowelIndex = word.IndexOf('a');
             }
-
-            if ( ( word.IndexOf('e') > -1 && word.IndexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
-            vowelIndex = word.IndexOf('e');
+            if ((word.IndexOf('e') > -1 && word.IndexOf('e') < vowelIndex) || vowelIndex == -1) {
+                vowelIndex = word.IndexOf('e');
             }
-
-            if ( ( word.IndexOf('i') > -1 && word.IndexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
-            vowelIndex = word.IndexOf('i');
+            if ((word.IndexOf('i') > -1 && word.IndexOf('i') < vowelIndex) || vowelIndex == -1) {
+                vowelIndex = word.IndexOf('i');
             }
-              if ( ( word.IndexOf('o') > -1 && word.IndexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
-            vowelIndex = word.indexOf('o');
+            if ((word.IndexOf('o') > -1 && word.IndexOf('o') < vowelIndex) || vowelIndex == -1) {
+                vowelIndex = word.IndexOf('o');
             }
-
-            if ( ( word.IndexOf('u') > -1 && word.IndexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
-            vowelIndex = word.IndexOf('u');
+            if ((word.IndexOf('u') > -1 && word.IndexOf('u') < vowelIndex) || vowelIndex == -1) {
+                vowelIndex = word.IndexOf('u');
             }
-
-            if ( ( word.IndexOf('y') > -1 && word.IndexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
-            vowelIndex = word.IndexOf('y');
+            if ((word.IndexOf('y') > -1 && word.IndexOf('y') < vowelIndex) || vowelIndex == -1) {
+                vowelIndex = word.IndexOf('y');
             }
-
             string firstPart = word.Substring(0, vowelIndex);
-            string restWord = word.substring(vowelIndex)
-             if (vowelIndex == 0) 
+            string secondPart = word.Substring(vowelIndex);
+            if (vowelIndex == 0) 
             {
                 word += "yay";
             } else {
                 word = secondPart + firstPart + "ay";
             }
-            Console.WriteLine(word);
+            string output = word.ToLower();
+            Console.WriteLine("Your Pig Latin Translation is:");
+            Console.WriteLine(output);
 
-            // leave this command at the end so your program does not close automatically
             Console.ReadLine();
-            }
+        }
         
         public static string TranslateWord(string word)
         {
