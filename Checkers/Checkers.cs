4e121a6a14c30f3efab51f2aaf19f5a8c7dbc7c5
Ukiah Richardson <urichardson74@gmail.com>
2018-06-08 00:57:53 -0500

    progress checkers

--- Checkers/Checkers.cs
+++ Checkers/Checkers.cs
@@ -8,50 +8,10 @@ namespace Checkers
     {
         static void Main(string[] args)
         {
-            int[][] whitePositions = new int[][] 
-            {
-                new int[] { 0, 1 }, new int[] { 0, 3 }, new int[] { 0, 5 }, new int[] { 0, 7 },
-                new int[] { 1, 0 }, new int[] { 1, 2 }, new int[] { 1, 4 }, new int[] { 1, 6 },
-                new int[] { 2, 1 }, new int[] { 2, 3 }, new int[] { 2, 5 }, new int[] { 2, 7 }
-            };
-
-            int[][] blackPositions = new int[][] 
-            {
-                new int[] { 5, 0 }, new int[] { 5, 2 }, new int[] { 5, 4 }, new int[] { 5, 6 },
-                new int[] { 6, 1 }, new int[] { 6, 3 }, new int[] { 6, 5 }, new int[] { 6, 7 },
-                new int[] { 7, 0 }, new int[] { 7, 2 }, new int[] { 7, 4 }, new int[] { 7, 6 }
-            };
-
-                int[][] squarePositions = new int[][] 
-            {
-                new int[] { 0, 0 }, new int[] { 0, 2 }, new int[] { 0, 4 }, new int[] { 0, 6 },
-                new int[] { 1, 1 }, new int[] { 1, 3 }, new int[] { 1, 5 }, new int[] { 1, 7 },
-                new int[] { 2, 0 }, new int[] { 2, 2 }, new int[] { 2, 4 }, new int[] { 2, 6 },
-                new int[] { 3, 1 }, new int[] { 3, 3 }, new int[] { 3, 5 }, new int[] { 3, 7 },
-                new int[] { 4, 0 }, new int[] { 4, 2 }, new int[] { 4, 4 }, new int[] { 4, 6 },
-                new int[] { 5, 1 }, new int[] { 5, 3 }, new int[] { 5, 5 }, new int[] { 5, 7 },
-                new int[] { 6, 0 }, new int[] { 6, 2 }, new int[] { 6, 4 }, new int[] { 6, 6 },
-                new int[] { 7, 1 }, new int[] { 7, 3 }, new int[] { 7, 5 }, new int[] { 7, 7 }
-            };
+           
 
             Board board = new Board();
-
-            for (int i = 0; i < 12; i++)
-            {
-                Checker white = new Checker("white", whitePositions[i]);
-                Checker black = new Checker("black", blackPositions[i]);
-                
-                board.Checkers.Add(white);
-                board.Checkers.Add(black);
-               
-            }
-
-            for (int i = 0; i < 32; i++)
-            {
-                Checker square = new Checker("square", squarePositions[i]); 
-                board.Checkers.Add(square);
-            }
-
+            board.GenerateCheckers();
             board.DrawBoard();
             Console.WriteLine("Select checker row:");
             int row = Convert.ToInt32(Console.ReadLine());
@@ -59,12 +19,20 @@ namespace Checkers
             int col = Convert.ToInt32(Console.ReadLine());
 
             Checker checker = board.SelectChecker(row, col);
+            Console.WriteLine("Please move checker to an empty space.");
             Console.WriteLine("Move to which row:");
             int newRow = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Move to which Column:");
             int newCol = Convert.ToInt32(Console.ReadLine());
 
-            checker.Position = new int[]{ newRow, newCol};
+            if (checker.Position == null)
+            {
+                checker.Position = new int[]{ newRow, newCol};
+            }
+            else
+            {
+                Console.WriteLine("Not a legal move");
+            }
             board.DrawBoard();
             Console.WriteLine("Hi");
            
@@ -91,7 +59,7 @@ namespace Checkers
         
             else 
             {
-            circleId= int.Parse("25A7", System.Globalization.NumberStyles.HexNumber);
+            circleId= int.Parse("25A9", System.Globalization.NumberStyles.HexNumber);
             }
             
            
@@ -133,7 +101,47 @@ namespace Checkers
         
         public void GenerateCheckers()
         {
-            // Your code here
+             int[][] whitePositions = new int[][] 
+            {
+                new int[] { 0, 1 }, new int[] { 0, 3 }, new int[] { 0, 5 }, new int[] { 0, 7 },
+                new int[] { 1, 0 }, new int[] { 1, 2 }, new int[] { 1, 4 }, new int[] { 1, 6 },
+                new int[] { 2, 1 }, new int[] { 2, 3 }, new int[] { 2, 5 }, new int[] { 2, 7 }
+            };
+
+            int[][] blackPositions = new int[][] 
+            {
+                new int[] { 5, 0 }, new int[] { 5, 2 }, new int[] { 5, 4 }, new int[] { 5, 6 },
+                new int[] { 6, 1 }, new int[] { 6, 3 }, new int[] { 6, 5 }, new int[] { 6, 7 },
+                new int[] { 7, 0 }, new int[] { 7, 2 }, new int[] { 7, 4 }, new int[] { 7, 6 }
+            };
+
+                int[][] squarePositions = new int[][] 
+            {
+                new int[] { 0, 0 }, new int[] { 0, 2 }, new int[] { 0, 4 }, new int[] { 0, 6 },
+                new int[] { 1, 1 }, new int[] { 1, 3 }, new int[] { 1, 5 }, new int[] { 1, 7 },
+                new int[] { 2, 0 }, new int[] { 2, 2 }, new int[] { 2, 4 }, new int[] { 2, 6 },
+                new int[] { 3, 1 }, new int[] { 3, 3 }, new int[] { 3, 5 }, new int[] { 3, 7 },
+                new int[] { 4, 0 }, new int[] { 4, 2 }, new int[] { 4, 4 }, new int[] { 4, 6 },
+                new int[] { 5, 1 }, new int[] { 5, 3 }, new int[] { 5, 5 }, new int[] { 5, 7 },
+                new int[] { 6, 0 }, new int[] { 6, 2 }, new int[] { 6, 4 }, new int[] { 6, 6 },
+                new int[] { 7, 1 }, new int[] { 7, 3 }, new int[] { 7, 5 }, new int[] { 7, 7 }
+            };
+
+             for (int i = 0; i < 12; i++)
+            {
+                Checker white = new Checker("white", whitePositions[i]);
+                Checker black = new Checker("black", blackPositions[i]);
+                
+                Checkers.Add(white);
+                Checkers.Add(black);
+               
+            }
+
+            for (int i = 0; i < 32; i++)
+            {
+                Checker square = new Checker("square", squarePositions[i]); 
+                Checkers.Add(square);
+            }
             return;
         }
         
